81e8db3d9a2a46fa2493292a8e770c2024057310
11Murad <ashrafov.main@gmail.com>
2025-03-24 02:59:56 +0400

    first commt

---
+++ src/main/java/com/spring/aiwebapp/AiWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.aiwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWebAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/aiwebapp/service/ChatService.java
@@ -0,0 +1,15 @@
+package com.spring.aiwebapp.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+    private final ChatModel chatModel;
+
+    public String getResponse(String prompt) {
+        return chatModel.call(prompt);
+    }
+}
