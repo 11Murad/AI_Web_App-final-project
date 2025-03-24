61f82465ca58a9660927e11e906939730aec6d90
11Murad <ashrafov.main@gmail.com>
2025-03-25 02:05:55 +0400

    Customize Controller for Images

--- src/main/java/com/spring/aiwebapp/controller/GenAIController.java
+++ src/main/java/com/spring/aiwebapp/controller/GenAIController.java
@@ -1,18 +1,25 @@
 package com.spring.aiwebapp.controller;
 
 import com.spring.aiwebapp.service.ChatService;
+import com.spring.aiwebapp.service.ImageService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping
 public class GenAIController {
     public final ChatService chatService;
+    public final ImageService imageService;
 
-    public GenAIController(ChatService chatService) {
+    public GenAIController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("/ask-ai")
@@ -23,6 +30,13 @@ public class GenAIController {
     @GetMapping("/ask-ai-options")
     public String getResponseOptions(@RequestParam String prompt) {
         return chatService.getResponseOptions(prompt);
+    }
+
+    @GetMapping("/generate-image")
+    public void generateImage(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String url = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(url);
     }
 
 
