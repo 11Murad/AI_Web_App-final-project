67445e93d4752db40b525d29acc4825a69bb99e8
11Murad <ashrafov.main@gmail.com>
2025-04-21 23:24:18 +0400

    adjustments

---
+++ src/main/java/com/spring/aiwebapp/entity/TextResponse.java
@@ -0,0 +1,49 @@
+package com.spring.aiwebapp.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@Table(name = "text_response")
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class TextResponse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "text_prompt_id", nullable = false)
+    private TextPrompt textPrompt;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
