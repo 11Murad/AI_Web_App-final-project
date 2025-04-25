5214d72321dce642300d8fa95e878fa6fa6c124f
11Murad <ashrafov.main@gmail.com>
2025-04-26 03:05:42 +0400

    AdminConfig

---
+++ src/main/java/com/spring/aiwebapp/config/AdminConfig.java
@@ -0,0 +1,33 @@
+package com.spring.aiwebapp.config;
+
+import com.spring.aiwebapp.entity.User;
+import com.spring.aiwebapp.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class AdminConfig {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    @Transactional
+    public void init() {
+        if (!userRepository.existsByEmail("ashrafov@gmail.com")) {
+            User admin = new User();
+            admin.setFirstName("Murad");
+            admin.setLastName("Ashrafov");
+            admin.setEmail("ashrafov@gmail.com");
+            admin.setPassword(passwordEncoder.encode("Murad.123"));
+            admin.setRoles(Set.of(User.Role.ROLE_ADMIN));
+            userRepository.save(admin);
+        }
+    }
+}
